c1d1e7f946243b97a10d8b6c4deaab39cb0341c4
Lamu Guo <lamuguo@gmail.com>
2023-05-04 16:06:58 -0700

    Make gorm support TLS connection with MySQL.

--- models/database.go
+++ models/database.go
@@ -1,11 +1,17 @@
 package models
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"database/sql"
 	"fmt"
+	mysqlDriver "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/spf13/viper"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
@@ -19,9 +25,32 @@ func autoMigrate(db *gorm.DB, entities ...interface{}) {
 }
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open(mysql.Open(viper.GetString("dbUrl")), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info), // Can set LogLevel here
-	})
+	enableCustomTls := viper.GetBool("enableCustomTls")
+	if enableCustomTls {
+		// Initialize connection string.
+		rootCertPool := x509.NewCertPool()
+		pem, err := ioutil.ReadFile(viper.GetString("pemPath"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
+			log.Fatal("Failed to append PEM.")
+		}
+		mysqlDriver.RegisterTLSConfig("custom", &tls.Config{RootCAs: rootCertPool})
+	}
+	dsn := viper.GetString("dsn")
+
+	// Open database with standard MySQL driver
+	sqlDb, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqlDb.Close()
+
+	// Pass sql.DB instance to GORM
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: sqlDb,
+	}), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)}) // Can set LogLevel here
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
