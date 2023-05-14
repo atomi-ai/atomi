fecbb86976fcdecd894efd1cdeb15d1b2d447a9d
Lamu Guo <lamuguo@gmail.com>
2023-05-14 10:04:18 -0700

    Added container sas token for end-to-end test.

--- exp/azure-upload/main.go
+++ exp/azure-upload/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	azureContainerURL := "https://atomidrone.blob.core.windows.net/images?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-04-16T23:57:30Z&st=2023-05-03T15:57:30Z&spr=https&sig=6rYp3%2Fak5JnN96%2BE5BmGDrzUyko1NRNg8xSp6p8CpXw%3D"
+	azureContainerURL := "https://atomidrone.blob.core.windows.net/testing?sp=racwdl&st=2023-05-01T16:59:44Z&se=2024-10-04T00:59:44Z&spr=https&sv=2022-11-02&sr=c&sig=NYvndFrlEpk1cvphkWLI9UEVlLIO2bghCqYVEWXwHyA%3D"
 	localFilePath := "/tmp/azure.exp"
 
 	abs, err := utils.NewAzureBlobStorage(azureContainerURL)
